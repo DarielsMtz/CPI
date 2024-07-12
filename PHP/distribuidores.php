aaf56873fdd82525342320e0b6c83e9a74325638
DarielsMtz <127892871+DarielsMtz@users.noreply.github.com>
2024-07-12 16:49:02 +0200

    feat: actualizar rutas de imágenes en CSS y PHP
    - Actualizar las rutas de las imágenes en el archivo CSS para reflejar la nueva ubicación de las imágenes en la carpeta "img/desktop".
    - Actualizar las rutas de las imágenes en el archivo PHP para reflejar la nueva ubicación de las imágenes en la carpeta "img/desktop/productos".
    
    Co-authored-by: [Author Name]

--- PHP/distribuidores.php
+++ PHP/distribuidores.php
@@ -1,6 +1,5 @@
  <script>
-     document.title = "Caribbean Probiotics - Disribuidores";
-
+document.title = "Caribbean Probiotics - Disribuidores";
  </script>
 
  <!-- Anclas Suaves -->
@@ -8,41 +7,40 @@
 
  <!-- OCULTAR ACORDEON ACTIVO -->
  <script>
-     /*OCULTAR ACORDEON ACTIVO*/
-     seeMoreOpened = false;
-     jQuery(document).ready(function($) {
-         var delay = 100;
-         setTimeout(function() {
-             $('.elementor-tab-title').removeClass('elementor-active');
-             $('.elementor-tab-content').css('display', 'none');
-         }, delay);
+/*OCULTAR ACORDEON ACTIVO*/
+seeMoreOpened = false;
+jQuery(document).ready(function($) {
+    var delay = 100;
+    setTimeout(function() {
+        $('.elementor-tab-title').removeClass('elementor-active');
+        $('.elementor-tab-content').css('display', 'none');
+    }, delay);
 
-         /*Mostrar/ocultar contenido*/
-         $('#elemento').hide();
+    /*Mostrar/ocultar contenido*/
+    $('#elemento').hide();
 
-         $('#boton_dis').click(function(event) {
-             $('#elemento').show(200);
-             event.preventDefault();
-         })
-     });
+    $('#boton_dis').click(function(event) {
+        $('#elemento').show(200);
+        event.preventDefault();
+    })
+});
 
-     /*Anclas*/
-     $(document).ready(function() {
-         $('a[href^="#"]').click(function() {
-             var destino = $(this.hash);
-             if (destino.length == 0) {
-                 destino = $('a[name="' + this.hash.substr(1) + '"]');
-             }
-             if (destino.length == 0) {
-                 destino = $('html');
-             }
-             $('html, body').animate({
-                 scrollTop: destino.offset().top - 90
-             }, 1300);
-             return false;
-         });
-     });
-
+/*Anclas*/
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+        var destino = $(this.hash);
+        if (destino.length == 0) {
+            destino = $('a[name="' + this.hash.substr(1) + '"]');
+        }
+        if (destino.length == 0) {
+            destino = $('html');
+        }
+        $('html, body').animate({
+            scrollTop: destino.offset().top - 90
+        }, 1300);
+        return false;
+    });
+});
  </script>
 
  <!-- Mapa Cabecera -->
@@ -69,26 +67,26 @@
 
      <div class="caja_dis" id="el_salvador">
          <h1> <a href="#elemento"> El Salvador </a> </h1>
-         <div class="logo_dis"> <img src="imagenes/logo_mallo.jpg" alt="Logo Grupo Mallo S.A"> </div>
+         <div class="logo_dis"> <img src="./img/desktop/logo_mallo.jpg" alt="Logo Grupo Mallo S.A"> </div>
          <li>
              <h3> GRUPO MALLO S.A. DE C.V. </h3>
-             <div class="icono"> <img src="imagenes/usuario.png">Contacto </div>
-             <div class="icono"> <img src="imagenes/llamada-telefonica.png">Telefono </div>
-             <div class="icono"> <img src="imagenes/email.png">Email </div>
-             <div class="icono"> <img src="imagenes/web.png">Web </div>
+             <div class="icono"> <img src="./img/desktop/usuario.png">Contacto </div>
+             <div class="icono"> <img src="./img/desktop/llamada-telefonica.png">Telefono </div>
+             <div class="icono"> <img src="./img/desktop/email.png">Email </div>
+             <div class="icono"> <img src="./img/desktop/web.png">Web </div>
          </li>
      </div>
 
      <hr>
      <div class="caja_dis" id="honduras">
          <h1><a href="#elemento"> Honduras </a> </h1>
-         <div class="logo_dis"> <img src="imagenes/logo_mallo.jpg" alt="Logo Mallo Honduras"> </div>
+         <div class="logo_dis"> <img src="./img/desktop/logo_mallo.jpg" alt="Logo Mallo Honduras"> </div>
          <li>
              <h3> MALLO HONDURAS S. DE R.L. </h3>
-             <div class="icono"> <img src="imagenes/usuario.png">Contacto </div>
-             <div class="icono"> <img src="imagenes/llamada-telefonica.png">Telefono </div>
-             <div class="icono"> <img src="imagenes/email.png">Email </div>
-             <div class="icono"> <img src="imagenes/web.png">Web </div>
+             <div class="icono"> <img src="./img/desktop/usuario.png">Contacto </div>
+             <div class="icono"> <img src="./img/desktop/llamada-telefonica.png">Telefono </div>
+             <div class="icono"> <img src="./img/desktop/email.png">Email </div>
+             <div class="icono"> <img src="./img/desktop/web.png">Web </div>
          </li>
 
      </div>
@@ -96,13 +94,13 @@
      <div class="caja_dis" id="guatemala">
          <h1> <a href="#elemento"> Guatemala </a> </h1>
          <div>
-             <div class="logo_dis"> <img src="imagenes/logo_semiagro%20(1).png" alt="Logo Semiagro"> </div>
+             <div class="logo_dis"> <img src="./img/desktop/logo_semiagro%20(1).png" alt="Logo Semiagro"> </div>
              <li>
                  <h3> Semillas e Insumos Agropecuarios S.A. </h3>
-                 <div class="icono"> <img src="imagenes/usuario.png">Contacto </div>
-                 <div class="icono"> <img src="imagenes/llamada-telefonica.png">Telefono </div>
-                 <div class="icono"> <img src="imagenes/email.png">Email </div>
-                 <div class="icono"> <img src="imagenes/web.png">Web </div>
+                 <div class="icono"> <img src="./img/desktop/usuario.png">Contacto </div>
+                 <div class="icono"> <img src="./img/desktop/llamada-telefonica.png">Telefono </div>
+                 <div class="icono"> <img src="./img/desktop/email.png">Email </div>
+                 <div class="icono"> <img src="./img/desktop/web.png">Web </div>
              </li>
          </div>
      </div>
@@ -110,13 +108,13 @@
      <!--     
      <div class="caja_dis" id="costa_rica">
          <h1> <a href="#elemento"> Costa Rica </a> </h1>
-         <div class="logo_dis"> <img src="imagenes/logo_mallo.jpg"> </div>
+         <div class="logo_dis"> <img src="./img/desktop/logo_mallo.jpg"> </div>
          <li>
              <h3> Mallo Honduras </h3>
-             <div class="icono"> <img src="imagenes/usuario.png">Contacto </div>
-             <div class="icono"> <img src="imagenes/llamada-telefonica.png">Telefono </div>
-             <div class="icono"> <img src="imagenes/email.png">Email </div>
-             <div class="icono"> <img src="imagenes/web.png">Web </div>
+             <div class="icono"> <img src="./img/desktop/usuario.png">Contacto </div>
+             <div class="icono"> <img src="./img/desktop/llamada-telefonica.png">Telefono </div>
+             <div class="icono"> <img src="./img/desktop/email.png">Email </div>
+             <div class="icono"> <img src="./img/desktop/web.png">Web </div>
          </li>
 
      </div>
@@ -124,39 +122,39 @@
 
      <div class="caja_dis" id="panama">
          <h1> <a href="#elemento"> Panamá </a> </h1>
-         <div class="logo_dis"> <img src="imagenes/logo_ageagro.png" alt="Logo Ageagro"> </div>
+         <div class="logo_dis"> <img src="./img/desktop/logo_ageagro.png" alt="Logo Ageagro"> </div>
          <li>
              <h3> DISTRIBUIDORA VETERINARIA AGEAGRO S.A. </h3>
-             <div class="icono"> <img src="imagenes/usuario.png">Contacto </div>
-             <div class="icono"> <img src="imagenes/llamada-telefonica.png">Telefono </div>
-             <div class="icono"> <img src="imagenes/email.png">Email </div>
-             <div class="icono"> <img src="imagenes/web.png">Web </div>
+             <div class="icono"> <img src="./img/desktop/usuario.png">Contacto </div>
+             <div class="icono"> <img src="./img/desktop/llamada-telefonica.png">Telefono </div>
+             <div class="icono"> <img src="./img/desktop/email.png">Email </div>
+             <div class="icono"> <img src="./img/desktop/web.png">Web </div>
          </li>
 
      </div>
      <hr>
      <div class="caja_dis" id="paraguay">
          <h1><a href="#elemento"> Paraguay </a> </h1>
-         <div class="logo_dis"> <img src="imagenes/logo_chimiray.png" alt="Logo Chimiray"> </div>
+         <div class="logo_dis"> <img src="./img/desktop/logo_chimiray.png" alt="Logo Chimiray"> </div>
          <li>
              <h3> CHIMIRAY S.A. </h3>
-             <div class="icono"> <img src="imagenes/usuario.png">Contacto </div>
-             <div class="icono"> <img src="imagenes/llamada-telefonica.png">Telefono </div>
-             <div class="icono"> <img src="imagenes/email.png">Email </div>
-             <div class="icono"> <img src="imagenes/web.png">Web </div>
+             <div class="icono"> <img src="./img/desktop/usuario.png">Contacto </div>
+             <div class="icono"> <img src="./img/desktop/llamada-telefonica.png">Telefono </div>
+             <div class="icono"> <img src="./img/desktop/email.png">Email </div>
+             <div class="icono"> <img src="./img/desktop/web.png">Web </div>
          </li>
 
      </div>
      <hr>
      <div class="caja_dis" id="argentina">
          <h1><a href="#elemento"> Argentina </a></h1>
-         <div class="logo_dis"> <img src="imagenes/logo_mayors2.png" alt="logo Mayors"> </div>
+         <div class="logo_dis"> <img src="./img/desktop/logo_mayors2.png" alt="logo Mayors"> </div>
          <li>
              <h3>MAYORS ESPECIALIDADES VETERINARIAS. </h3>
-             <div class="icono"> <img src="imagenes/usuario.png">Contacto </div>
-             <div class="icono"> <img src="imagenes/llamada-telefonica.png">Telefono </div>
-             <div class="icono"> <img src="imagenes/email.png">Email </div>
-             <div class="icono"> <img src="imagenes/web.png">Web </div>
+             <div class="icono"> <img src="./img/desktop/usuario.png">Contacto </div>
+             <div class="icono"> <img src="./img/desktop/llamada-telefonica.png">Telefono </div>
+             <div class="icono"> <img src="./img/desktop/email.png">Email </div>
+             <div class="icono"> <img src="./img/desktop/web.png">Web </div>
          </li>
 
      </div>
--- PHP/productos.php
+++ PHP/productos.php
@@ -1,7 +1,6 @@
 <!-- Titulo de la pagina -->
 <script>
-    document.title = "Caribbean Probiotics - Productos";
-
+document.title = "Caribbean Probiotics - Productos";
 </script>
 
 <div class="titulo_pro"> Productos </div>
@@ -9,91 +8,91 @@
 <!-- Productos-->
 <div class="products">
     <div class="product">
-        <img src="imagenes/productos/Producto%20(1).png" alt="Viyo Recuperación Perro">
+        <img src="img/desktop/productos/Producto%20(1).png" alt="Viyo Recuperación Perro">
         <h3>Viyo Recuperation Perro </h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(2).png" alt="Viyo Recuperación Gato">
+        <img src="img/desktop/productos/Producto%20(2).png" alt="Viyo Recuperación Gato">
         <h3>Viyo Recuperation Gato </h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(3).png" alt="Viyo Fortalece Perro">
+        <img src="img/desktop/productos/Producto%20(3).png" alt="Viyo Fortalece Perro">
         <h3>Viyo Fortalece Perro</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(4).png" alt="Viyo Fortalece Gato">
+        <img src="img/desktop/productos/Producto%20(4).png" alt="Viyo Fortalece Gato">
         <h3>Viyo Fortalece Gato</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(9).png" alt="Viyo Imune+ Perro">
+        <img src="img/desktop/productos/Producto%20(9).png" alt="Viyo Imune+ Perro">
         <h3>Viyo Fortalece Perro</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(8).png" alt="Viyo Imune+ Gato">
+        <img src="img/desktop/productos/Producto%20(8).png" alt="Viyo Imune+ Gato">
         <h3>Viyo Fortalece Perro</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(6).png" alt="Calmin Senior">
+        <img src="img/desktop/productos/Producto%20(6).png" alt="Calmin Senior">
         <h3> Calmi Senior </h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(7).png" alt="Calmin">
+        <img src="img/desktop/productos/Producto%20(7).png" alt="Calmin">
         <h3> Calmin Perro</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(13).png" alt="Ferma Dol Forte">
+        <img src="img/desktop/productos/Producto%20(13).png" alt="Ferma Dol Forte">
         <h3>Ferma Dol Forte</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(12).png" alt="Viyo Fortalece Perro">
+        <img src="img/desktop/productos/Producto%20(12).png" alt="Viyo Fortalece Perro">
         <h3>Primalac</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(10).png" alt="Viyo Fortalece Perro">
+        <img src="img/desktop/productos/Producto%20(10).png" alt="Viyo Fortalece Perro">
         <h3>Primalac Aves</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(11).png" alt="Viyo Fortalece Perro">
+        <img src="img/desktop/productos/Producto%20(11).png" alt="Viyo Fortalece Perro">
         <h3>Sila-Prime</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
     </div>
 
     <div class="product">
-        <img src="imagenes/productos/Producto%20(5).png" alt="BioAg">
+        <img src="img/desktop/productos/Producto%20(5).png" alt="BioAg">
         <h3>BioAg</h3>
         <p>Este producto está diseñado solo para alimentación intermitente o suplementaria.</p>
         <a href="index.php?content=ViyoRep"> Ver Más</a>
